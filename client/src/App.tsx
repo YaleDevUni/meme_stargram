654da476e3f133948987795d64e2f2f19dceeca3
Suhun Kim <kksshh404@gmail.com>
2022-11-27 14:24:31 -0800

    start point

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Navbar } from './component/navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Header
+        <Navbar is_signed_in={false} name={"Paul"} new_alarm={3}/>
       </header>
     </div>
   );
