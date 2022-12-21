2711fa9a7862709fd07b69fccfb6424c72ba969f
YaleDevUni <rhrnak4403@gmail.com>
2022-12-20 21:33:46 -0800

    from prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,16 @@
 // import React from 'react';
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import GroupList from './components/group_list';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import GroupList from "./components/group_list";
 // import './App.css';
 
 function App() {
   return (
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <p className="text-3xl text-gray-700 font-bold mb-5">
-        Welcome!
-      </p>
-      <p className="text-gray-500 text-lg">
-        React and Tailwind CSS in action
-      </p>
+    <div className="container m-10 mx-auto rounded-xl border bg-gray-200 p-8 shadow">
+      <p className="mb-5 text-3xl font-bold text-gray-700">Welcome!</p>
+      <p className="text-lg text-gray-500">React and Tailwind CSS in action</p>
       <GroupList />
     </div>
-  
-  
   );
 }
 
