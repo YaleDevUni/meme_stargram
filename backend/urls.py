720975cb7f605bca71876fb343ae5137e25eb34b
Suhun Kim <kksshh404@gmail.com>
2022-12-24 15:20:11 -0800

    empty comments

--- backend/urls.py
+++ backend/urls.py
@@ -1,3 +1,8 @@
+#######################################################
+# Combined url configuration to pass to the URL config
+# This gathers all urls from all apps living in backend
+#######################################################
+
 from rest_framework import routers
 from posts.views import PostViewset
 from users.views import UserViewset
