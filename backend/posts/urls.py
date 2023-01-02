7477f5e8903001fad0731b3c62916fcd87d5a915
YaleDevUni <rhrnak4403@gmail.com>
2023-01-01 17:37:32 -0800

    commit 1

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -1,7 +1,10 @@
 from rest_framework import routers
 from .views import PostViewset
+from django.urls import path
 
 router = routers.DefaultRouter()
 router.register('posts', PostViewset, basename='posts')
 
 urlpatterns = router.urls
+
+
