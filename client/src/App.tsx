08ce54f2d53b7753fdbc5b559dd75696da10b7b2
Min Gu Kim <mingukim0505@gmail.com>
2023-01-03 02:40:00 -0800

    branch change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import GroupList from './components/group_list';
 import Modal from './components/modal';
-import Navbar from './components/navbar';
+// import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="container m-10 mx-auto rounded-xl border bg-gray-200 p-8 shadow">
       <p className="mb-5 text-3xl font-bold text-gray-700">Welcome!</p>
       <p className="text-lg text-gray-500">React and Tailwind CSS in action</p>
-      {/* <Navbar /> */}
+
       <Modal />
       <GroupList />
 
