cf252e0d442a04670e75c3191eaed1765b1fe5df
Min Gu Kim <mingukim0505@gmail.com>
2023-01-02 23:35:54 -0800

    changing branch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,15 @@ function App() {
       <p className="mb-5 text-3xl font-bold text-gray-700">Welcome!</p>
       <p className="text-lg text-gray-500">React and Tailwind CSS in action</p>
       <Modal />
-      <GroupList />
+      {/* <GroupList /> */}
+      <div className="... columns-3">
+        <img className="... aspect-video w-full" src="..." />
+        <img className="... aspect-square w-full" src="..." />
+        <img className="... aspect-square w-full" src="..." />
+        <img className="... aspect-square w-full" src="..." />
+        <img className="... aspect-square w-full" src="..." />
+        <img className="... aspect-square w-full" src="..." />
+      </div>
     </div>
   );
 }
