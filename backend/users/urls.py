96d43adec4c7698822de5709260e170fda44508f
YaleDevUni <rhrnak4403@gmail.com>
2023-01-07 00:36:24 -0800

    feature/auth

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path,include
 from rest_framework import routers
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -12,7 +12,8 @@ router.register('users', views.UserViewset, basename='users')
 
 urlpatterns = router.urls
 urlpatterns += [
-    path('jwt/create/',TokenObtainPairView, name='jwt_create'),
-    path('jwt/refresh/',TokenRefreshView, name='jwt_refresh'),
-    path('jwt/verify/',TokenVerifyView, name='jwt_verify')
+    
+    # path('jwt/create/',TokenObtainPairView, name='jwt_create'),
+    # path('jwt/refresh/',TokenRefreshView, name='jwt_refresh'),
+    # path('jwt/verify/',TokenVerifyView, name='jwt_verify')
 ]
