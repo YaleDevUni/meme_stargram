eaf970003b76f64a357f1bb403bcdfe636b20d01
YaleDevUni <rhrnak4403@gmail.com>
2023-02-12 19:33:27 -0800

    empty

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+
 import PostList from './components/group_list';
 import Navbar from './components/navbar';
 function App() {
