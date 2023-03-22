b48c36d41c9b4d9b17c6a784f318d52c6ab9134d
YaleDevUni <rhrnak4403@gmail.com>
2023-03-22 10:11:28 -0700

    discard exp_searchbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-
-import PostList from './components/group_list';
 import Navbar from './components/navbar';
+import PostList from './components/group_list';
+// import Navbar from './components/navbar';
 function App() {
   return (
     <div>
